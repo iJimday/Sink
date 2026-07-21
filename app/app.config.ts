f8b975493402f9de887327488236320188d7898b
Tree New Bee <ijimday@gmail.com>
2026-07-21 23:01:29 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,10 +1,10 @@
 export default defineAppConfig({
   title: 'JimDay Links',
-  documentation: ' ',
+  documentation: 'https://sink.cool/',
   github: 'https://github.com/iJimday/Sink',
-  coffee: 'https://jimday.cn/',
-  twitter: 'https://jimday.cn/',
-  telegram: 'https://jimday.cn/',
+  coffee: 'https://www.jimday.cn/',
+  twitter: 'https://www.jimday.cn/',
+  telegram: 'https://www.jimday.cn/',
   description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
